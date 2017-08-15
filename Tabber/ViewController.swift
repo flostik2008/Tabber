f0188a8625d55340d5275b92673c563ef8a0a980
Evgeny Vlasov <flostik2008@gmail.com>
2017-08-14 19:15:27 -0700

    UIv2.0 Update

--- Tabber/ViewController.swift
+++ Tabber/ViewController.swift
@@ -10,24 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var tipLbl: UILabel!
     @IBOutlet weak var totalLbl: UILabel!
     @IBOutlet weak var billField: UITextField!
     @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var justTipLbl: UILabel!
+    @IBOutlet weak var controllerToField: NSLayoutConstraint!
+    @IBOutlet weak var for2PeopleLbl: UILabel!
+    @IBOutlet weak var for3PeopleLbl: UILabel!
+    @IBOutlet weak var for4PeopleLbl: UILabel!
     
     var tipPercent: Double!
     var tipValue: Double!
+    var opened = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         tipControl.addTarget(self, action:#selector(calcTip), for: .valueChanged)
-
         updateValues()
+        
+        billField.becomeFirstResponder()
+        
+        billField.attributedPlaceholder = NSAttributedString(string: "$", attributes: [NSForegroundColorAttributeName: UIColor(red:0.00, green:0.52, blue:1.00, alpha:1.0)])
+        
+        navigationController?.navigationBar.barTintColor = UIColor(red:0.00, green:0.52, blue:1.00, alpha:1.0)
+        navigationController?.navigationBar.tintColor = UIColor(red:0.51, green:0.74, blue:0.91, alpha:1.0)
+    
+        navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.white]
+        navigationController?.navigationBar.titleTextAttributes = [NSFontAttributeName: UIFont(name: "HelveticaNeue-ThinItalic", size:30)!]
+        
+        
+        
     }
 
     override func viewWillAppear(_ animated: Bool) {
-       
+        self.controllerToField.constant = 300
+        self.view.layoutIfNeeded()
+
         updateValues()
     }
     
@@ -37,6 +56,21 @@ class ViewController: UIViewController {
     }
     
     @IBAction func calcTip(_ sender: Any) {
+        
+        if billField.text != ""{
+        UIView.animate(withDuration: 0.5, animations: {
+            self.controllerToField.constant = 20
+            self.view.layoutIfNeeded()
+            self.opened = true
+        })
+        } else if billField.text == "" && opened == true {
+            UIView.animate(withDuration: 0.5, animations: {
+                self.controllerToField.constant = 300
+                self.view.layoutIfNeeded()
+                self.opened = false
+            })
+
+        }
         
         let tipArray = [0.18, 0.20, 0.25]
         
@@ -49,9 +83,11 @@ class ViewController: UIViewController {
         tipValue = bill * tipArray[tipControl.selectedSegmentIndex]
         
         let total = bill + tipValue
-        tipLbl.text = String(format: "$%.2f", tipValue)
+        justTipLbl.text = String(format: "$%.2f", tipValue)
         totalLbl.text = String(format: "$%.2f", total)
- 
+        for2PeopleLbl.text = String(format: "$%.2f", total/2)
+        for3PeopleLbl.text = String(format: "$%.2f", total/3)
+        for4PeopleLbl.text = String(format: "$%.2f", total/4)
     }
     
     func updateValues() {
@@ -60,7 +96,6 @@ class ViewController: UIViewController {
         tipPercent = defaults.double(forKey: "default_tip")
  
         let roundedTip = Double(round(100*tipPercent)/100)
-        print("Zhenya: \(roundedTip)")
         
         if roundedTip == 0.18 {
             tipControl.selectedSegmentIndex = 0
